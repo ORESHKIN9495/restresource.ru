1cc41812f7df8c58637f36352ba508ac3dead6b2
ORESHKIN9495 <orehmoskow@gmail.com>
2023-02-24 19:45:44 +0300

    update Signin

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -9,54 +9,87 @@ const router = useRouter();
 const selectedItem = ref();
 const disabled = ref(false);
 
+const regExpUser = /^[a-z0-9_-]{3,16}$/;
+const regExpPass =
+  /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{6,}/g;
+
 const setActive = (index: any) => (selectedItem.value = index);
 
-const userData = ref({
+const errRegExpUser = ref(false);
+const errRegExpPass = ref(false);
+
+const userData = {
   username: {
     value: "",
-    placeholder: "Username or Email Address",
+    placeholder: "Username ",
     err: false,
+    type: "text",
   },
   password: {
     value: "",
     placeholder: "Password",
     err: false,
+    type: "text",
   },
-});
+};
 
-const invalid = () => {
-  if (userData.value.username.value == "") {
-    userData.value.username.err = true;
+const validation = () => {
+  if (userData.username.value == "") {
+    userData.username.err = true;
+    userData.username.placeholder = "Поле должго быть заполнено";
     disabled.value = true;
-  } else if (userData.value.password.value == "") {
-    userData.value.password.err = true;
+  } else if (!regExpUser.test(userData.username.value)) {
+    disabled.value = true;
+    userData.username.err = true;
+    errRegExpUser.value = true;
+  } else {
+    userData.username.err = false;
+    errRegExpUser.value = false;
+    userData.username.placeholder = "Username ";
+    disabled.value = false;
+  }
+
+  if (userData.password.value == "") {
+    userData.password.err = true;
+    userData.password.placeholder = "Поле должго быть заполнено";
+    disabled.value = true;
+  } else if (!regExpPass.test(userData.password.value)) {
+    userData.password.err = true;
+    errRegExpPass.value = true;
     disabled.value = true;
   } else {
-    userData.value.username.err = false;
-    userData.value.password.err = false;
+    userData.password.err = false;
+    errRegExpPass.value = false;
+    userData.password.placeholder = "Password";
     disabled.value = false;
-    postData();
+
+    localStorage.setItem("username", userData.username.value);
+    localStorage.setItem("password", userData.password.value);
+    router.push({ path: "/" });
   }
 };
 
 const postData = () => {
-  userData.value.username.value = userData.value.password.value = "";
+  validation();
 };
 </script>
 
 <template>
   <section>
-    <form action="#" method="POST">
+    <form action="#" method="POST" @submit.prevent="postData">
       <h1>Sign In</h1>
 
       <input
         v-for="(i, index) in userData"
-        type="text"
+        :type="i.type"
         :placeholder="i.placeholder"
         v-model.trim="i.value"
         :class="{ active: index == selectedItem, err: i.err }"
         @click="setActive(index)"
       />
+
+      <p v-if="errRegExpUser">Введите корректное значение имени пользователя</p>
+      <p v-if="errRegExpPass">Введите корректное значение пароля</p>
 
       <span>
         <TheButton
@@ -64,7 +97,6 @@ const postData = () => {
           :g="true"
           type="submit"
           :disabled="disabled == !disabled"
-          @click.prevent="invalid()"
         />
         <TheButton
           title="Cancel"
@@ -125,6 +157,10 @@ section {
       &.err {
         border-color: #fa0202;
         box-shadow: 0 19px 28px -18px #fa02027b;
+
+        &::placeholder {
+          color: #fa0202;
+        }
       }
     }
 
